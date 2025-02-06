31ed9133eb46688f23ee84efc4efb0770bb6024b
jebin2 <jebineinstein@gmail.com>
2025-02-06 20:04:39 +0530

    small change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="custom_logger",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
     install_requires=[
         "pygame",
     ],
